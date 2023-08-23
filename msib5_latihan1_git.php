ae87cf49978f09d67bed22704ca946a2a2a30d23
greensand01 <sjsamuels01@gmail.com>
2023-08-23 22:36:54 +0800

    first commit

---
+++ msib5_latihan1_git.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="container">
+        <h1>Form Data Mahasiswa</h1>
+        <hr>
+        <table>
+            <tr>
+                <td><b>Nama</b></td>
+                <td>:</td>
+                <td>Saputra Jonatan Samuels</td>
+            </tr>
+            <tr>
+                <td><b>Jurusan</b></td>
+                <td>:</td>
+                <td>Teknik Informatika</td>
+            </tr>
+            <tr>
+                <td><b>Asal Universitas</b></td>
+                <td>:</td>
+                <td>Universitas Negeri Manado</td>
+            </tr>
+
+            
+                <td><b>Hobby</b></td>
+                <td></td>
+                <td>
+                    <li>Nonton Anime</li>
+                    <li>Main Game (kecuali bola)</li>
+                   
+                </td>
+           
+        </table>
+    </div>
+
+<style>
+
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    max-width: 600px;
+    margin: 50px auto;
+    padding: 20px;
+    background-color: #ffffff;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+}
+
+table {
+    display: grid;
+    gap: 10px;
+}
+
+
+
+
+
+</style>
+
+</body>
+</html>
